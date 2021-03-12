b210fce39ebf7b372c1619d8f7e01b27c07c044b
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-03-12 14:41:04 +0300

    refactor: split code into modules

---
+++ js/util.js
@@ -0,0 +1,30 @@
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+};
+
+
+const checkStringLength = (string, length) => string.length <= length;
+checkStringLength; // for ESLint Validation
+
+//Создает массив из уникальных чисел в указанном диапазоне указанной длины
+const getUniqueRandomNumbers = (length, min, max) => {
+  let arr = [];
+  while (arr.length < length) {
+    const number = getRandomInt(min, max);
+    if (arr.indexOf(number) === -1) arr.push(number);
+  }
+  return arr;
+}
+// Функция возвращающает случайное значение из массива
+const getRandomArrayElement = (array) => {
+  const randomNumber = Math.floor(Math.random() * array.length);
+  return array[randomNumber];
+};
+
+export {
+  getUniqueRandomNumbers,
+  getRandomArrayElement,
+  getRandomInt
+};
